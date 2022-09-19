f7759b66c994e1863d31257d224975bc5d437c88
D4t0rn03rd <81825575+D4t0rn03rd425@users.noreply.github.com>
2022-09-19 18:16:46 +0200

    RoundF2022: enhanced fabric task afterwards

---
+++ RoundF2022/sortFabrics/README.md
@@ -0,0 +1,5 @@
+# description of my solution(s)
+
+When looking at the test results after the event I realized that I only forgot to cast the values to an Integer when sorting by durability as requested for Charles.
+Therefore, [fabric_test_set_1.py](fabric_test_set_1.py) is the solution I handed in during the event which passed test set 1.
+The adapted version [fabric_test_set_2.py](fabric_test_set_2.py) can not be considered as a valid attempt as it was handed in after the event, but it also passes test set 2 ;)
---
+++ RoundF2022/sortFabrics/fabric_test_set_2.py
@@ -0,0 +1,30 @@
+import collections
+
+t = int(input())
+
+for i in range(t):
+    n = int(input())
+    
+    fabrics = []
+    for j in range(n):
+        fabrics.append(input())
+
+    doubleList = []
+    for j in range(len(fabrics)):
+        doubleList.append(fabrics[j].split(' '))
+
+    sortedDoubleList = sorted(doubleList, key=lambda x: int(x[2]))
+
+    # Ada
+    adaDoubleList = sorted(sortedDoubleList, key=lambda x: x[0])
+
+    # Charles
+    charlesDoubleList = sorted(sortedDoubleList, key=lambda x: int(x[1]))
+
+    k = 0
+
+    for j in range(len(doubleList)):
+        if adaDoubleList[j] == charlesDoubleList[j]:
+            k += 1    
+
+    print(f"Case #{i + 1}: {k}")
